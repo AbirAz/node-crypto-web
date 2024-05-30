f2aa56d81d160c1a1156400c9e43e18d1008afc6
AbirAz <avirazstudy@gmail.com>
2024-05-30 10:17:33 +0300

    adding jsons

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,9 @@
+import config from 'config';
 import express from 'express';
 
 const app = express();
 
-app.listen(3000, () => {
-    console.log('app started...')
+//  NODE_ENV=staging npm start
+app.listen(config.get('app.port'), () => {
+    console.log(`${config.get(`app.name`)} started | with secret ${config.get(`app.secret`)} | in ENV:${config.get(`app.ENV`)} | on port:${config.get(`app.port`)}`)
 })

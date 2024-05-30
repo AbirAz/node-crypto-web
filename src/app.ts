6dfe3dc578f8bf1543633486db4aa9908ab1c237
AbirAz <avirazstudy@gmail.com>
2024-05-30 10:40:12 +0300

    add dashboard page

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,15 @@
 import config from 'config';
 import express from 'express';
+import usersRouter from './routers/users'
+import path from 'path';
 
 const app = express();
+app.set('view engine', 'ejs')
+app.set('views', path.resolve(__dirname, 'views'))
+
+app.use('/users', usersRouter);
 
 //  NODE_ENV=staging npm start
 app.listen(config.get('app.port'), () => {
-    console.log(`${config.get(`app.name`)} started | with secret ${config.get(`app.secret`)} | in ENV:${config.get(`app.ENV`)} | on port:${config.get(`app.port`)}`)
+    console.log(`${config.get(`app.name`)} started! | SECRET: ${config.get(`app.secret`)} | ENV:${config.get(`app.ENV`)} | PORT:${config.get(`app.port`)}`)
 })
---
+++ src/routers/users.ts
@@ -0,0 +1,17 @@
+import { Router } from "express"
+
+const router = Router();
+
+router.get('/dashboard', (req,res) => {
+    res.render('users/dashboard')
+})
+
+router.get('/dashboard', (req,res) => {
+    res.render('users/dashboard')
+})
+
+router.get('/dashboard', (req,res) => {
+    res.render('users/dashboard')
+})
+
+export default router;

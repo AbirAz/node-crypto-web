9427e223c38d34dc235d6d6204e7598f2cb25cbf
AbirAz <avirazstudy@gmail.com>
2024-06-02 10:56:17 +0300

    money to web

--- src/controllers/users/controller.ts
+++ src/controllers/users/controller.ts
@@ -1,13 +1,20 @@
 import { NextFunction, Request, Response } from "express";
 import { DTO } from "../../models/user-symbol/dto";
 import getModel from "../../models/user-symbol/factory";
+import getSymbolValueModel from "../../models/symbol-value/factory";
 
 
 export async function dashboard(req: Request, res: Response, next: NextFunction){
     try{
         const userSymbols = await getModel().getForUser(1);
+
+        const symbolValues = await Promise.all( 
+            userSymbols.map(({ symbol }) => getSymbolValueModel().getLatest(symbol))
+        )
+
         res.render(`users/dashboard`,{
-            userSymbols
+            userSymbols,
+            symbolValues
         })
     }catch(err){
         next(err);
